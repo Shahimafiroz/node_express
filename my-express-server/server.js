e743903b4bdd4d607bdd6ff573cfef6f25943c8e
SHAHIMA FIROZ KHAN <99420269+Shahimafiroz@users.noreply.github.com>
2022-12-14 21:37:46 +0530

    after a long time

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -2,8 +2,26 @@ const exp = require("express");
 var app = exp();
 
 // respond req 1
-app.get("/", function (request, respond) {
-  respond.send("hello guys ... not happy to see u!!!");
+app.get("/", function (req, res) {
+  res.send("<h1>hello guys ... not happy to see u!!!</h1>");
+  console.log(req);
+});
+
+app.get("/contact", function (req, res) {
+  res.send("<h2> khanshahima4@gmail.com </h2>");
+  console.log(req);
+});
+
+app.get("/about", function (req, res) {
+  res.send(
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+  );
+  console.log(req);
+});
+
+app.get("/contact/phone", function (req, res) {
+  res.send("<h2> 8605410290 </h2>");
+  console.log(req);
 });
 
 // // respond req 1
