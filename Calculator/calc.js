35600bd3d8635d4ed0bf79fed606d5a71e2a6735
SHAHIMA FIROZ KHAN <99420269+Shahimafiroz@users.noreply.github.com>
2022-12-16 14:12:33 +0530

    complete calc

--- Calculator/calc.js
+++ Calculator/calc.js
@@ -1,13 +1,19 @@
 const exp = require("express");
-const app = exp();
+
 const bodyp = require("body-parser");
+const app = exp();
+app.use(bodyp.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/", function (req, res) {
-  res.send("hbxcjdh");
+  console.log(req.body);
+  var no1 = Number(req.body.num1);
+  var no2 = Number(req.body.num2);
+  var rese = no1 + no2;
+  res.send("Result is " + rese);
 });
 
 app.listen(3000, function () {
