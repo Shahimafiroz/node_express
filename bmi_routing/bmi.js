be20b1833d516bb17d988b303f973f96625e7371
SHAHIMA FIROZ KHAN <99420269+Shahimafiroz@users.noreply.github.com>
2022-12-17 23:47:15 +0530

    finishgit add .

--- bmi_routing/bmi.js
+++ bmi_routing/bmi.js
@@ -2,3 +2,19 @@ var exp = require("express");
 var bp = require("body-parser");
 
 var app = exp();
+app.use(bp.urlencoded({ extended: true }));
+
+app.get("/", function (rq, rs) {
+  rs.sendFile(__dirname + "/index.html");
+});
+app.post("/", function (rq, rs) {
+  console.log(rq.body);
+  var h = rq.body.h;
+  var w = rq.body.w;
+  var bmi = w / (h * h);
+  rs.send("ur bmi is " + bmi);
+  // rs.sendFile(__dirname + "/table.html" + "ur bmi is " + bmi);
+});
+app.listen(3000, function () {
+  console.log("SERVER RUNNING ON PORT 3000");
+});
